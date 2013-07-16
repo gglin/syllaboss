02070b7e969a165200553fec8a57ef85fe7cd0cb
Jen E <jeneisenberg5@gmail.com>
2013-07-16 13:26:31 -0400

    paragraphized schedule

--- app/models/school_day.rb
+++ app/models/school_day.rb
@@ -1,4 +1,11 @@
 class SchoolDay < ActiveRecord::Base
   belongs_to :potd
   attr_accessible :calendar_date, :ordinal, :schedule, :week
+
+
+	def paragraphize(content)
+		body = content.split("\n").collect{|paragraph| "<p>#{paragraph}</p>"}.join("\n")
+		body
+	end
+
 end

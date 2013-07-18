b43b51b935fca74b621aafdff577120f078c8b3f
Jen E <jeneisenberg5@gmail.com>
2013-07-18 11:00:16 -0400

    formattable helper all working

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,6 +1,6 @@
 class Todo < ActiveRecord::Base
   include Formattable
-  attr_accessible :gist, :name
+  attr_accessible :gist, :name, :school_day_ids
 
   has_many :school_day_todos
   has_many :school_days, :through => :school_day_todos

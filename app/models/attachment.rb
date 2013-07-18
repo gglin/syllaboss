e2420293cd90c00d495e94a8b84fb7cd6f20522c
gglin <george.g.lin@gmail.com>
2013-07-18 17:59:43 -0400

    Carrierwave works

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -1,4 +1,6 @@
 class Attachment < ActiveRecord::Base
   attr_accessible :attachable_id, :attachable_type, :filename, :title
   belongs_to :attachable, polymorphic: true
+
+  mount_uploader :filename, FileUploader
 end

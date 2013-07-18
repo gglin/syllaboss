4d6ba4f3804e0a883608e06880f37629b6c2956a
Chris Gonzales <gonzales.chris.a@gmail.com>
2013-07-18 16:14:19 -0400

    simplify school_day index, add potd/labs to this index w/ directory routing

--- app/models/lab.rb
+++ app/models/lab.rb
@@ -1,6 +1,6 @@
 class Lab < ActiveRecord::Base
   include Formattable
-  attr_accessible :lab_url, :name
+  attr_accessible :lab_url, :name, :school_day_ids
 
   has_many :school_day_labs
   has_many :school_days, :through => :school_day_labs
--- app/models/link.rb
+++ app/models/link.rb
@@ -5,13 +5,7 @@ class Link < ActiveRecord::Base
   has_many :school_day_links
   has_many :school_days, :through => :school_day_links
 
-  def http_link
-   if link_url[0..3] != "http"
-      link_url.prepend("http://")
-   else
-      link_url
-   end
-  end
+
 
 end
 

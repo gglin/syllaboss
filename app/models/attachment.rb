dedb34566403a1eeba80966658554b0dd096df38
gglin <george.g.lin@gmail.com>
2013-07-19 12:55:58 -0400

    Attachments belong to lectures polymorphically

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -1,6 +1,12 @@
 class Attachment < ActiveRecord::Base
-  attr_accessible :attachable_id, :attachable_type, :filename, :title
+  belongs_to :lecture
   belongs_to :attachable, polymorphic: true
 
+  attr_accessible :attachable_id, :attachable_type, :filename, :title
+
   mount_uploader :filename, FileUploader
+
+  def filename_short
+    filename.to_s.split("/").last
+  end
 end
--- app/models/lecture.rb
+++ app/models/lecture.rb
@@ -1,11 +1,15 @@
 class Lecture < ActiveRecord::Base
   attr_accessible :content, :creator, :file_upload, :title
-  attr_accessible :school_day_ids   # these columns do not exist in db - only for mass assign
+  attr_accessible :school_day_ids, :attachment_ids   # these columns do not exist in db - only for mass assign
 
   has_many :school_day_lectures
   has_many :school_days, :through => :school_day_lectures
 
   has_many :attachments, as: :attachable
+  # has_many :attachables, :through => :attachment, :as => :attachable
 
-  # mount_uploader :file_upload, AttachmentUploader
+  def paragraphize(content)
+    body = content.split("\n").collect{|paragraph| "<p>#{paragraph}</p>"}.join("\n")
+    body
+  end
 end

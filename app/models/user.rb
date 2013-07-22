1cb483776de28ee0a863f6df03da290d7118a51e
Jen E <jeneisenberg5@gmail.com>
2013-07-22 09:51:28 -0400

    start sessions model

---
+++ app/models/user.rb
@@ -0,0 +1,45 @@
+class User < ActiveRecord::Base
+  has_secure_password
+
+  attr_accessible :email, :role, :password, :password_confirmation, :password_digest
+  validates :password, :presence => { :on => :create }
+  validates_uniqueness_of :email, :message => "Sorry, that e-mail address already belongs to an existing account."
+
+    USER_ROLES = {
+    :admin => 0,
+    :student => 10
+  }
+
+
+  def set_as_admin 
+    self.role = USER_ROLES[:admin]
+  end
+
+  def set_as_student 
+    self.role = USER_ROLES[:student]
+  end
+  
+  def admin?
+    true if self.role_name == :admin
+  end
+
+  def owns?(school_day)
+    true if self.id == school_day.user_id
+  end
+
+  def self.admin
+    User.where(:role => USER_ROLES[:admin])
+  end
+
+  def role_name
+    User.user_roles.key(self.role)
+  end
+
+  def can_edit?(school_day)
+    true if admin? || owns?(school_day)
+  end
+
+  def can_destroy?(school_day)
+    true if admin? || owns?(school_day)
+  end
+end

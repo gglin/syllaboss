9b01b8aaa28fd892796989b4e07c82c2ed3442fb
Jen E <jeneisenberg5@gmail.com>
2013-07-22 17:20:07 -0400

    user model connects to comments

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130722181224) do
+ActiveRecord::Schema.define(:version => 20130722211509) do
 
   create_table "attachments", :force => true do |t|
     t.string   "title"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(:version => 20130722181224) do
     t.string   "commentable_type"
     t.datetime "created_at",       :null => false
     t.datetime "updated_at",       :null => false
+    t.integer  "user_id"
   end
 
   add_index "comments", ["commentable_id", "commentable_type"], :name => "index_comments_on_commentable_id_and_commentable_type"
@@ -131,6 +132,8 @@ ActiveRecord::Schema.define(:version => 20130722181224) do
     t.datetime "created_at",      :null => false
     t.datetime "updated_at",      :null => false
     t.integer  "role"
+    t.string   "full_name"
+    t.string   "username"
   end
 
 end

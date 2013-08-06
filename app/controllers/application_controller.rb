18c78ce4ab00b62aff0c4c282dcbbd003aa10c40
Jen E <jeneisenberg5@gmail.com>
2013-08-06 12:13:51 -0400

    notification turned off to future days in student view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -39,8 +39,19 @@ private
 
   def load_unread_resources
     @unread = []
-    material_type_list.each do |material|
-      @unread += material[:name].classify.constantize.unread_by(current_user) if material[:name] != "school_day"
+    material_type_list.each do |material_hash|
+      material_type = material_hash[:name]
+      if material_type != "school_day"
+        unread_materials = material_type.classify.constantize.unread_by(current_user)
+        if current_user.admin?
+          filtered_unread_materials = unread_materials
+        else
+          filtered_unread_materials = unread_materials.select do |material| 
+            !material.school_days.empty? && !material.school_days.nil? && material.school_days.order("calendar_date")[0].calendar_date <= Date.today
+          end
+        end
+        @unread += filtered_unread_materials
+      end
     end
     @unread.sort_by!(&:updated_at).reverse!
   end

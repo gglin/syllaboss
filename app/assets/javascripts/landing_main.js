1874b00b300c75e355b51441f48606f27e133c1a
gglin <george.g.lin@gmail.com>
2013-08-07 14:58:22 -0400

    Link at bottom of form block stays fixed

---
+++ app/assets/javascripts/landing_main.js
@@ -0,0 +1,62 @@
+var buffercheck;
+var videobuffer;
+var video = document.getElementById('background-video');
+var refreshcount = 0;
+
+// Video and Audio
+
+$(document).ready(function(){
+	
+	mute();
+
+	video.play();
+
+	$('.mute').click(function(e){
+		switchAudio();
+		return false;
+	});
+
+});
+
+function switchAudio() {
+	//if video is muted, unmute and show audo on button and vice versa
+	if(video.muted) {
+		unMute();
+	}
+	else {
+		mute();
+	}
+}
+
+function unMute(){
+	ga('send', 'Audio', 'Unmute');
+	video.muted = false;
+	$('.mute').text('Mute');	
+}
+
+function mute(){
+	ga('send', 'Audio', 'Mute');
+	video.muted = true;
+	$('.mute').text('Play Sound');	
+}
+
+
+// Show / Hide Login / Signup Forms
+
+$('.signup-button').click(function(e){
+    e.preventDefault();
+    var $showform = $('.signup-form');
+    var $hideform = $('.login-form');
+           
+    $showform.toggle(0);
+    $hideform.toggle(0);
+
+    if($showform.is(':visible')) 
+    {
+      $(this).text("Log In")
+    }
+    else 
+    {
+      $(this).text("Sign Up")
+    } 
+  }); 

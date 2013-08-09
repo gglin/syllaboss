4aa6344d71ceb6e8bfc94db36e65c67bbd40f476
Chris Gonzales <gonzales.chris.a@gmail.com>
2013-08-09 11:15:25 -0400

    lucky friday commit

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,12 +1,15 @@
 CarrierWave.configure do |config|
   if Rails.env.production?
-    config.storage = :s3
-    config.s3_access_key_id = 'ACCESSKEY'
-    config.s3_secret_access_key = 'SEKRIT'
-    config.s3_bucket = 'i_haz_bukkit'
-    config.s3_access = :public_read
-    config.s3_headers = {'Cache-Control' => 'max-age=315576000', 
-       'Expires' => 99.years.from_now.httpdate}
+    # config.storage = :s3
+    # config.s3_access_key_id = 'ACCESSKEY'
+    # config.s3_secret_access_key = 'SEKRIT'
+    # config.s3_bucket = 'i_haz_bukkit'
+    # config.s3_access = :public_read
+    # config.s3_headers = {'Cache-Control' => 'max-age=315576000', 
+    #    'Expires' => 99.years.from_now.httpdate}
+    config.root = Rails.root.join('tmp')
+    config.storage = :file
+    
   elsif Rails.env.development?
     config.storage = :file
   else

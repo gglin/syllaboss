c1493c46ad228e8dbc1fce475b58ec050d1fb214
gglin <george.g.lin@gmail.com>
2013-08-17 14:26:06 -0400

    Auth token in cookies instead of user ID, remember me, forgotten password

---
+++ app/controllers/password_resets_controller.rb
@@ -0,0 +1,35 @@
+class PasswordResetsController < ApplicationController
+
+  skip_before_filter :authenticate
+  skip_before_filter :load_unread_resources 
+  skip_before_filter :load_all_unread       
+  skip_before_filter :load_unread_comments  
+  skip_before_filter :load_announcements    
+
+  def new
+    render :layout => "landing"
+  end
+
+  def create
+    user = User.find_by_email(params[:email])
+    user.send_password_reset if user
+    redirect_to login_url, :notice => "Email sent with password reset instructions."
+  end
+
+  def edit
+    @user = User.find_by_password_reset_token!(params[:id])
+    render :layout => "landing"
+  end
+
+  def update
+    @user = User.find_by_password_reset_token!(params[:id])
+    if @user.password_reset_sent_at < 2.hours.ago
+      redirect_to new_password_reset_path, :alert => "Password reset has expired."
+    elsif @user.update_attributes(params[:user])
+      redirect_to login_url, :notice => "Password has been reset!"
+    else
+      render :edit
+    end
+  end
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130813184301) do
+ActiveRecord::Schema.define(:version => 20130817154642) do
 
   create_table "abilities", :force => true do |t|
     t.datetime "created_at", :null => false
@@ -152,19 +152,18 @@ ActiveRecord::Schema.define(:version => 20130813184301) do
   create_table "users", :force => true do |t|
     t.string   "email"
     t.string   "password_digest"
-    t.datetime "created_at",          :null => false
-    t.datetime "updated_at",          :null => false
+    t.datetime "created_at",             :null => false
+    t.datetime "updated_at",             :null => false
     t.integer  "role"
     t.string   "full_name"
     t.string   "username"
-    t.string   "avatar_file_name"
-    t.string   "avatar_content_type"
-    t.integer  "avatar_file_size"
-    t.datetime "avatar_updated_at"
     t.string   "image"
     t.string   "facebook"
     t.string   "linkedin"
     t.string   "twitter"
+    t.string   "auth_token"
+    t.string   "password_reset_token"
+    t.datetime "password_reset_sent_at"
   end
 
 end

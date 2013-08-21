61f97ba5bbd3425dca2a0561cb70e154a4943b16
surrealdetective <thefifth@gmail.com>
2013-08-20 20:00:12 -0400

    configures rspec-rails and factory girl to avoid superfluous functionality from these gems

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,18 @@ module Pizza
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # Specifies settings for rspec-rails and factory_girl_rails
+    config.generators do |g|
+      g.test_framework :rspec,
+        fixtures: true,
+        view_specs: false,
+        helper_specs: false,
+        routing_specs: false,
+        controller_specs: true,
+        request_specs: false
+      g.fixture_replacement :factory_girl, dir: "spec/factories"
+    end
+    
   end
 end

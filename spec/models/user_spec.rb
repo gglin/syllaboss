b2056530ccab35720fc3933b27ed6406de5d3b64
surrealdetective <thefifth@gmail.com>
2013-08-20 20:46:23 -0400

    describes future tests for user model

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,9 @@
+describe User do
+  it "is valid with a full name, email, and password"
+  it "is not valid without a full name"
+  it "is not valid without an email"
+  it "is not valid without a password"
+  it "is invalid with a duplicate email address"
+  it 'prints its username thru print_name'
+end
+
